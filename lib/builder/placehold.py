e893093da97bf303dffb70fedf4ed50f79c2979d
nanakenashi <nanakenashi@gmail.com>
2016-08-15 00:58:47 +0900

    Added libraries

---
+++ lib/builder/placehold.py
@@ -0,0 +1,45 @@
+import os
+
+class Placehold:
+
+    ENDPOINT = 'https://placehold.jp'
+
+    @classmethod
+    def build_url(cls, opts):
+        directory = cls.__directory(opts)
+        file_name = cls.__file_name(opts)
+        path = os.path.join(cls.ENDPOINT, directory, file_name)
+
+        query_string = cls.__query_string(opts)
+
+        return path + query_string
+
+    @staticmethod
+    def __directory(opts):
+        parts = [
+            opts.get('text_size', None),
+            opts.get('bg_color', None),
+            opts.get('text_color', None)
+        ]
+        necessary = [part for part in parts if part is not None]
+
+        return '/'.join(necessary)
+
+    @staticmethod
+    def __file_name(opts):
+        width  = opts.get('width', 150)
+        height = opts.get('height', 150)
+        format = opts.get('format', 'png')
+
+        return "{width}x{height}.{format}".format(**vars())
+
+    @staticmethod
+    def __query_string(opts):
+        text = opts.get('text', None)
+
+        if text is not None:
+            query_string = "?text={text}".format(**vars())
+        else:
+            query_string = ''
+
+        return query_string
---
+++ lib/collector.py
@@ -0,0 +1,19 @@
+from urllib import request
+from builder.placehold import Placehold
+
+class Collector:
+
+    # TODO: assign dynamically builder class
+    def __init__(self, builder_name='placehold'):
+        self.builder = Placehold
+
+    def collect(self, opts, output_path):
+        url = self.builder.build_url(opts)
+        self.__save_content(url, output_path)
+
+    def __save_content(self, url, output_path):
+        with request.urlopen(url) as response:
+            content = response.read()
+
+            with open(output_path, 'wb') as file:
+                file.write(content)
